cba0a0f95a0cbef63315bb6f497edc6d237df0c1
TheKolmik <namexin@seznam.cz>
2022-08-05 07:08:58 +0200

    3.9.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,15 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+// import FavoritesEmptyPage from '../../pages/favorites-empty-page/favorites-empty-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
+// import MainEmptyPage from '../../pages/main-empty-page/main-empty-page';
 import MainPage from '../../pages/main-page/main-page';
+// import PropertyNotLoggedPage from '../../pages/property-not-logged-page/property-not-logged';
+import Property from '../../pages/property-page/property-page';
+import Page404 from '../404-page/404-page';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import PrivateRoute from '../../components/private-route';
+
 
 type AppScreenProps = {
   places: number;
@@ -6,7 +17,34 @@ type AppScreenProps = {
 
 function App({places}: AppScreenProps): JSX.Element {
   return (
-    <MainPage places={places} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainPage places={places} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <FavoritesPage/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Property/>}
+        />
+        <Route
+          path="*"
+          element={<Page404/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
